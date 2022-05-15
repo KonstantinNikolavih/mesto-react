edab6eb14a96627583ad8e4b571732cb9f8d6969
KonstantinNikolavih <82166023+KonstantinNikolavih@users.noreply.github.com>
2022-05-15 12:21:10 +0300

    app

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
+import { style } from "../App.module.css";
+
+export const App = () => {
+
+
+
+
+  return (
+    <>
+      <div className={style.app}>
+
+      </div>
+    </>
+  )
+}
